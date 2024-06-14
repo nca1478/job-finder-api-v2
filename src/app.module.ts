ece1437d94f1fdf56579f3ca78229440ea13cc11
Nelson Cadenas <nca1478@yahoo.com>
2024-06-14 15:45:21 -0400

    added: UsersModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './common/database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
@@ -8,6 +9,7 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
     }),
     DatabaseModule,
+    UsersModule,
   ],
 })
 export class AppModule {}
