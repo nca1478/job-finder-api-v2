ac636e4d45848ef2da87e1370f92f5809e797334
Nelson Cadenas <nca1478@yahoo.com>
2024-06-20 09:06:09 -0400

    moved: regex code into utils folder

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -8,7 +8,7 @@ import {
   MinLength,
 } from 'class-validator';
 import { UserModel } from '../models/user.model';
-import { regExp } from '../../../common/regex';
+import { regExp } from '../../../utils/regex';
 
 export class CreateUserDto implements UserModel {
   @IsNotEmpty({ message: 'El nombre es requerido' })
