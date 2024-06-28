4ed5c28407e0179c279b3a61a304452d27bf1c64
Nelson Cadenas <nca1478@yahoo.com>
2024-06-28 12:42:48 -0400

    added: public static files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { DatabaseModule } from './common/database/database.module';
 import { UsersModule } from './modules/users/users.module';
 import { EnvConfigModule } from './common/env-config/env-config.module';
@@ -12,6 +14,10 @@ import { AuthModule } from './modules/auth/auth.module';
 
     EnvConfigModule.forRoot({
       isGlobal: true,
+    }),
+
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
     }),
 
     UsersModule,
