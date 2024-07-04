8d6b78eeb15b47792ad43c6a2c461516981b0062
Nelson Cadenas <nca1478@yahoo.com>
2024-07-04 17:00:06 -0400

    added: global-config function

---
+++ src/global-config.ts
@@ -0,0 +1,46 @@
+import * as passport from 'passport';
+import * as session from 'express-session';
+import { HttpAdapterHost } from '@nestjs/core';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { EnvConfigService } from './common/env-config';
+import { UnauthorizedExceptionFilter } from './common/errors/filters/unauthorized-exception/unauthorized-exception.filter';
+import { ResponseInterceptor } from './common/interceptors/response/response.interceptor';
+import { DatabaseInterceptor } from './common/errors/interceptors/database.interceptor';
+
+export function applyGlobalConfig(app: INestApplication) {
+  const adapterHost = app.get(HttpAdapterHost);
+  const configService = app.get<EnvConfigService>(EnvConfigService);
+
+  app.enableCors();
+
+  app.setGlobalPrefix('api/v2');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.useGlobalFilters(new UnauthorizedExceptionFilter(adapterHost));
+
+  app.useGlobalInterceptors(
+    new ResponseInterceptor(),
+    new DatabaseInterceptor(),
+  );
+
+  app.use(
+    session({
+      secret: configService.getApiSessionKey(),
+      saveUninitialized: false,
+      resave: false,
+      cookie: {
+        maxAge: 60000,
+      },
+    }),
+  );
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+}
--- src/main.ts
+++ src/main.ts
@@ -1,48 +1,13 @@
-import * as passport from 'passport';
-import * as session from 'express-session';
-import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { DatabaseInterceptor } from './common/errors/interceptors/database.interceptor';
-import { ResponseInterceptor } from './common/interceptors/response/response.interceptor';
-import { UnauthorizedExceptionFilter } from './common/errors/filters/unauthorized-exception/unauthorized-exception.filter';
-import { EnvConfigService } from './common/env-config';
+import { applyGlobalConfig } from './global-config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const adapterHost = app.get(HttpAdapterHost);
-  const configService = app.get<EnvConfigService>(EnvConfigService);
 
-  app.enableCors();
-
-  app.setGlobalPrefix('api/v2');
-
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-    }),
-  );
-
-  app.useGlobalFilters(new UnauthorizedExceptionFilter(adapterHost));
-  app.useGlobalInterceptors(new ResponseInterceptor());
-  app.useGlobalInterceptors(new DatabaseInterceptor());
-
-  app.use(
-    session({
-      secret: configService.getApiSessionKey(),
-      saveUninitialized: false,
-      resave: false,
-      cookie: {
-        maxAge: 60000,
-      },
-    }),
-  );
-
-  app.use(passport.initialize());
-  app.use(passport.session());
+  applyGlobalConfig(app);
 
   await app.listen(3000);
 }
+
 bootstrap();
