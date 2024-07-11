b877de806f236fa67494ba6f42ba30d5e6beb4dd
Nelson Cadenas <nca1478@yahoo.com>
2024-07-11 16:56:50 -0400

    added: app logger (on main.ts)

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
+
 import { AppModule } from './app.module';
 import { applyGlobalConfig } from './global-config';
 import { EnvConfigService } from './common/env-config';
@@ -6,10 +8,13 @@ import { EnvConfigService } from './common/env-config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get<EnvConfigService>(EnvConfigService);
+  const logger = new Logger('Bootstrap');
 
   applyGlobalConfig(app);
 
   await app.listen(configService.getAppPort());
+
+  logger.log(`App running on port ${configService.getAppPort()}`);
 }
 
 bootstrap();
