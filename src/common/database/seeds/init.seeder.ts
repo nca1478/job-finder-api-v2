370b05743f4fb7ece92f53fce41c4820ce6a0dcd
Nelson Cadenas <nca1478@yahoo.com>
2024-07-16 20:40:43 -0400

    added: logger on InitSeeder

--- src/common/database/seeds/init.seeder.ts
+++ src/common/database/seeds/init.seeder.ts
@@ -3,8 +3,11 @@ import { runSeeders, Seeder, SeederFactoryManager } from 'typeorm-extension';
 
 import userFactory from '../factories/users/user.factory';
 import { SectorSeeder, SkillSeeder, UserSeeder } from './index';
+import { Logger } from '@nestjs/common';
 
 export default class InitSeeder implements Seeder {
+  private readonly logger = new Logger('InitSeeder');
+
   public async run(
     dataSource: DataSource,
     factoryManager: SeederFactoryManager,
@@ -13,5 +16,7 @@ export default class InitSeeder implements Seeder {
       seeds: [UserSeeder, SkillSeeder, SectorSeeder],
       factories: [userFactory],
     });
+
+    this.logger.log('Seed ejecutado exitosamente');
   }
 }
