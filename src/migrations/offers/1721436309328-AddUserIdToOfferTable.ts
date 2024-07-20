d330db40504d9cbf17dcb5772f08fb275ac69d1e
Nelson Cadenas <nca1478@yahoo.com>
2024-07-19 20:56:13 -0400

    added: userId to offer table (migration)

---
+++ src/migrations/offers/1721436309328-AddUserIdToOfferTable.ts
@@ -0,0 +1,36 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export class AddUserIdToOfferTable1721436309328 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'offers',
+      new TableColumn({
+        name: 'userId',
+        type: 'uuid',
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'offers',
+      new TableForeignKey({
+        columnNames: ['userId'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'users',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    const table = await queryRunner.getTable('offers');
+    const foreignKey = table.foreignKeys.find(
+      (fk) => fk.columnNames.indexOf('userId') !== -1,
+    );
+    await queryRunner.dropForeignKey('offers', foreignKey);
+    await queryRunner.dropColumn('offers', 'userId');
+  }
+}
