b23b31d009a138a252118662b1a47fab8418709f
Nelson Cadenas <nca1478@yahoo.com>
2024-07-19 20:26:23 -0400

    added: offers entity migration

---
+++ src/migrations/offers/1721434233883-CreateOfferTable.ts
@@ -0,0 +1,71 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+import { basicPropertiesMigration } from '../../common/database/basic-properties.migration';
+import { UUID_EXTENSION } from '../../common/constants';
+
+export class CreateOfferTable1721434233883 implements MigrationInterface {
+  private tableName = 'offers';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(UUID_EXTENSION);
+
+    await queryRunner.createTable(
+      new Table({
+        name: this.tableName,
+        columns: [
+          ...basicPropertiesMigration,
+          {
+            name: 'title',
+            type: 'varchar',
+          },
+          {
+            name: 'description',
+            type: 'text',
+          },
+          {
+            name: 'country',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'state',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'city',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'price',
+            type: 'numeric',
+            default: 0,
+          },
+          {
+            name: 'currency',
+            type: 'varchar',
+          },
+          {
+            name: 'img',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'published',
+            type: 'varchar',
+            default: false,
+          },
+          {
+            name: 'active',
+            type: 'boolean',
+            default: true,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable(this.tableName);
+  }
+}
