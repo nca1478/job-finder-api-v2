327f3a2f231d816e85ffb21e6bcbca43ff8b1195
Nelson Cadenas <nca1478@yahoo.com>
2024-07-23 17:20:27 -0400

    added: basic setting unit testing (UsersService)

--- src/modules/users/services/__tests__/users.service.spec.ts
+++ src/modules/users/services/__tests__/users.service.spec.ts
@@ -1,10 +1,34 @@
+import { randomUUID } from 'crypto';
 import { UsersService } from '../users.service';
 
 describe('UsersService unit tests', () => {
   let service: UsersService;
+  let id: string;
+  let createdAt: Date;
+  let expectOutputUsers: any;
+  let mockUserRepository: any;
 
   beforeEach(async () => {
     service = new UsersService();
+    id = randomUUID();
+    createdAt = new Date();
+
+    expectOutputUsers = {
+      id,
+      name: 'Test',
+      email: 'test@gmail.com',
+      password: '123456Pass$$',
+      createdAt,
+    };
+
+    mockUserRepository = {
+      create: jest.fn().mockReturnValue(Promise.resolve(expectOutputUsers)),
+      save: jest.fn().mockReturnValue(Promise.resolve(expectOutputUsers)),
+      update: jest.fn().mockReturnValue(Promise.resolve(expectOutputUsers)),
+      preload: jest.fn().mockReturnValue(Promise.resolve(expectOutputUsers)),
+      findOne: jest.fn().mockReturnValue(Promise.resolve(expectOutputUsers)),
+      remove: jest.fn().mockReturnValue(Promise.resolve(expectOutputUsers)),
+    };
   });
 
   it('should be defined ', () => {
